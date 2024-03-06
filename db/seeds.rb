785cd6a9c85814e230474d6800a636663c9ec0be
gabi <gabrielteodorescu2212@yahoo.com>
2024-03-06 12:53:08 +0100

    Faker + Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: categories.sample
+  )
+end
